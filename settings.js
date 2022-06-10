1370ebc6a9ba8a8d448c0707cecbf62215a22fce
subagyoevendi <106004746+subagyoevendi@users.noreply.github.com>
2022-06-10 13:48:07 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,9 +9,9 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //Ubah Disini
-global.ownerNumber = ['6285609233482', '0']
-global.ownerName = 'Fandyyy X All Creator'
-global.packname = 'Ichigo Kurosaki'
+global.ownerNumber = ['6289522183114', '0']
+global.ownerName = 'miacas'
+global.packname = 'mia cans'
 global.author = 'Whatsapp Bot 2022'
 global.prefa = ['','!','.','#','!'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
 global.sessionName = 'sesi'
